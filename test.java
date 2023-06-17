5bce47e4bc503630dd4a32d2da8029d64dcb8165
tabrikian <117291931+tabrikian@users.noreply.github.com>
2023-06-17 13:02:18 +0300

    Update test.java

--- test.java
+++ test.java
@@ -1,34 +1,47 @@
 public class test {
     public static void printHeap(BinomialHeap heap){
-        System.out.println(heap.size);
-        System.out.println(heap.numTrees);
-        if (heap.last == null)
+        System.out.println("size: " + heap.size);
+        System.out.println("num of trees: " + heap.numTrees);
+        if (heap.empty())
             return;
-        System.out.println(heap.min.item.key);
-        String[][] toPrint = new String[(int) Math.pow(2, heap.last.rank)][heap.last.rank+1];
+        System.out.println("Min: " + itemToString(heap.min.item));
+        System.out.println("Last.rank: " +heap.last.rank);
+        System.out.println("Last: " +itemToString(heap.last.item));
+        System.out.println();
+        System.out.println();
+
+        String[][] toPrint = new String[heap.last.rank+1][(int) Math.pow(2, heap.last.rank)];
+        for (int i = 0; i < toPrint.length; i++){
+            for (int j = 0; j < toPrint[0].length; j++){
+                toPrint[i][j] = "";
+            }
+        }
         int offset = 0;
         BinomialHeap.HeapNode node = heap.last;
         do {
             node = node.next;
             offset += printTree(node, toPrint, offset, 0);
         }while (node != heap.last);
-        int[] lens = new int[(int) Math.pow(2, heap.last.rank)];
+
+        int[] lens = new int[toPrint[0].length];
         for(int i = 0; i < lens.length; i++){
-            for(int j = 0; j < toPrint[0].length; j++){
-                lens[i] = Math.max(lens[i], toPrint[i][j].length());
+            for (String[] strings : toPrint) {
+                lens[i] = Math.max(lens[i], strings[i].length());
             }
         }
-        for(int i = 0; i < toPrint[0].length; i++){
-            for(int j = 0; j < lens.length; j++){
-                System.out.print(toPrint[j][i]);
-                for(int k = 0; k < lens[i] - toPrint[j][i].length(); k++)
+        for (String[] strings : toPrint) {
+            for (int j = 0; j < toPrint[0].length; j++) {
+                System.out.print(strings[j]);
+                for (int k = 0; k < lens[j] - strings[j].length() + 3; k++)
                     System.out.print(' ');
             }
-            System.out.println(' ');
+            System.out.println();
         }
+        System.out.println();
+        System.out.println();
     }
     public static int printTree(BinomialHeap.HeapNode tree, String[][] toPrint, int offsetX, int offsetY){
-        toPrint[offsetX][offsetY] = toString(tree.item);
+        toPrint[offsetY][offsetX] = itemToString(tree.item);
         if(tree.child != null){
             int offset = 0;
             BinomialHeap.HeapNode node = tree.child;
@@ -39,13 +52,18 @@ public class test {
         }
         return 1;
     }
-    public static String toString(BinomialHeap.HeapItem item){
-        return "<" + Integer.toString(item.key) + ", " + item.info + ">";
+    public static String itemToString(BinomialHeap.HeapItem item){
+        return "<" + item.key + ", " + item.info + ">";
     }
     public static void main(String[] args){
         BinomialHeap heap = new BinomialHeap();
-        heap.insert(1, "abc");
-        heap.insert(2, "abcd");
+        for(int i = 1; i < 10; i++){
+            String str = "";
+            for (int j = 0; j < i; j++){
+                str = str + "a";
+            }
+            heap.insert(i, str);
+        }
         printHeap(heap);
     }
 }
